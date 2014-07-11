2f2f3d0ce73a348deb72440d84b1f47031c6def1
Naoki Iwane <niwane@expedia.com>
2014-07-11 09:50:17 -0700

    commit

--- src/main/java/com/naoki/filter/ItineraryAuthenticationFilter.java
+++ src/main/java/com/naoki/filter/ItineraryAuthenticationFilter.java
@@ -1,7 +1,19 @@
 package com.naoki.filter;
 
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.PreMatching;
+import java.io.IOException;
+
 /**
  * Created by niwane on 7/10/14.
  */
-public class ItineraryAuthenticationFilter {
+@PreMatching
+public class ItineraryAuthenticationFilter implements ContainerRequestFilter {
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+
+    }
 }
